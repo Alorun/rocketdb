47ed835ad9aaaf1f78a1e03f50d2db33073f3592
hahaha <a310608318@gmail.com>
2026-03-03 14:04:24 +0800

    add status

--- include/comparator.h
+++ include/comparator.h
@@ -13,8 +13,12 @@ public:
 
     virtual const char* Name() const = 0;
 
-    virtual void FindShortestSperator(std::string* start, const Slice& limit) const = 0;
+    // By comparing letter before and after,
+    // the size of the index term can be reduced without affecting the comparison order
+    virtual void FindShortestSeparator(std::string* start, const Slice& limit) const = 0;
 
+    // By defining a maximum index for a block,
+    // lookups can be sped up, while the length of the last index can also be reduced
     virtual void FindShortSuccessor(std::string* key) const = 0;
 };
 
