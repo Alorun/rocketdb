f51f4ae65367510cddbb4a3a1978d603dd8625e1
hahaha <a310608318@gmail.com>
2026-03-28 23:00:55 +0800

    finish version_edit

---
+++ src/db/version_set.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <map>
+#include <set>
+#include <vector>
+
+#include "dbformat.h"
+#include "version_edit.h"
+#include "../port/port.h"
+#include "../port/thread_annotations.h"
+
+namespace rocketdb {
+
+namespace log {
+    class Writer;
+}
+
+
+
+}
--- src/util/coding.h
+++ src/util/coding.h
@@ -16,6 +16,7 @@ void PutFixed64(std::string* dst, uint64_t value);
 void PutVarint32(std::string* dst, uint32_t value);
 void PutVarint64(std::string* dst, uint64_t value);
 
+// First add length, then add the value
 void PutLengthPrefixedSlice(std::string* dst, const Slice& value);
 
 bool GetVarint32(Slice* input, uint32_t* value);
