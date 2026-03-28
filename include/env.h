b0c41cf94704fde8285689881f4e2da6d00b00a4
hahaha <a310608318@gmail.com>
2026-03-28 15:18:18 +0800

    add content env.h finish filename add version

--- include/env.h
+++ include/env.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <cstdint>
 
+#include "options.h"
 #include "slice.h"
 #include "status.h"
 
@@ -14,6 +15,61 @@ namespace rocketdb {
 class SequentialFile;
 class Slice;
 class WritableFile;
+class RandomAccessFile;
+class FileLock;
+
+class Env {
+    public:
+        Env();
+
+        Env(const Env&) = delete;
+        Env& operator=(const Env&) = delete;
+
+        virtual ~Env();
+
+        static Env* Default();
+
+        virtual Status NewSequentialFile(const std::string& fname, SequentialFile** result) = 0;
+
+        virtual Status NewRandomAccessFile(const std::string& fname, RandomAccessFile** result) = 0;
+
+        virtual Status NewWriteFile(const std::string& fname, WritableFile** result) = 0;
+
+        virtual Status NewAppendableFile(const std::string& fname, WritableFile** result) = 0;
+
+        virtual bool FileExists(const std::string& fname) = 0;
+
+        virtual Status GetChildren(const std::string& dir, std::vector<std::string>* result) = 0;
+
+        virtual Status RemoveFile(const std::string fname);
+
+        virtual Status CreateDir(const std::string& dirname);
+
+        virtual Status RemoveDir(const std::string& dirname);
+
+        virtual Status DeleteDir(const std::string& dirname);
+
+        virtual Status GetFileSize(const std::string& fname, uint64_t* file_size) = 0;
+
+        virtual Status RenameFile(const std::string& src, const std::string& target) = 0;
+
+        virtual Status LockFile(const std::string& fname, FileLock** lock) = 0;
+
+        virtual Status UnlockFile(const FileLock* lock) = 0;
+
+        virtual void Schedule(void (*function)(void* arg), void* arg) = 0;
+
+        virtual void StartThread(void (*function)(void* arg), void* arg) = 0;
+
+        virtual Status GetTestDiredtory(std::string* path) = 0;
+
+        virtual Status NewLogger(const std::string& fname, Logger** result) = 0;
+
+        virtual uint64_t NowMicros() = 0;
+
+        virtual void SleepForMicroseconds(int micros) = 0;
+};  
+
 
 class SequentialFile {
     public:
---
+++ src/db/filename.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+#include "../../include/slice.h"
+#include "../../include/status.h"
+#include "../../include/env.h"
+#include "../port/port.h"
+
+namespace rocketdb {
+
+enum FileType {
+    kLogFile,
+    kDBLockFile,
+    kTableFile,
+    kDescriptorFile,
+    kCurrentFile,
+    kTempFile,
+    kInfoLogFile
+};
+
+std::string LogFileName(const std::string& dbname, uint64_t number);
+
+std::string TableFileName(const std::string& dbname, uint64_t number);
+
+std::string SSTTableFileName(const std::string& dbname, uint64_t number);
+
+std::string TempFileName(const std::string& dbname, uint64_t number);
+
+std::string DescriptorFileName(const std::string& dbname, uint64_t number);
+
+// Single file, don't need number
+std::string CurrentFileName(const std::string& dbname);
+
+std::string LockFileName(const std::string& dbname);
+
+std::string InfoLogFileName(const std::string& dbname);
+
+std::string OldInfoLogFileName(const std::string& dbname);
+
+bool ParseFileName(const std::string& filename, uint64_t* number, FileType* type);
+
+Status SetCurrentFile(Env* env, const std::string& dbname, uint64_t descriptor_number);
+
+}
